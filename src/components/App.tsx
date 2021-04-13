21c2c20e975e842623e130cc080fcf3c98fb7a50
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-04-13 22:45:36 +0200

    initialized project

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import Layout from "./layout";
+
+function App() {
+  return (
+    <div className="App">
+      <Layout/>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "./footer";
+import Header from "./header";
+import Main from "./main";
+
+function Layout() {
+    return (
+        <div>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    )
+}
+export default Layout;
