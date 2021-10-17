fc840f387bc71f5f9e3d929dbc7c8e1d88c81c39
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-10-17 20:27:36 +0200

    implement new header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,34 @@
+import styled from "styled-components";
+import PexelsStallning from "../assets/pexelsStallning.jpg";
 
 function Header() {
-    return(
-        <header>
-            <h1>hello world</h1>
-        </header>
-    )
+  return (
+    <header>
+      <StyledContainer>
+        <StyledTextContainer>von Braun Utbildning</StyledTextContainer>
+      </StyledContainer>
+    </header>
+  );
 }
-export default Header; 
+export default Header;
+
+const StyledContainer = styled.div`
+  background-image: url(${PexelsStallning});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-y: bottom;
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  justify-content: end;
+`;
+
+const StyledTextContainer = styled.div`
+  background: rgba(255, 255, 255, 0.6);
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
