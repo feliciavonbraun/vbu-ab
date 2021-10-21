cb345d83bc917f652840b70ff4df7a473c84696d
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-10-21 18:43:06 +0200

    replace photogallery with a carousel

---
+++ src/components/main/imageCarousel.tsx
@@ -0,0 +1,56 @@
+import { Box } from "@material-ui/core";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import styled from "styled-components";
+import gallerys from "../interfaces/galleryArray";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      flexWrap: "nowrap",
+      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+      transform: "translateZ(0)",
+    },
+    title: {
+      color: theme.palette.primary.light,
+    },
+    titleBar: {
+      background:
+        "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+    },
+  })
+);
+
+function ImageCarousel() {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <StyledH2>GALLRI</StyledH2>
+      <GridList className={classes.gridList} cols={4}>
+        {gallerys.map((gallery, index) => (
+          <GridListTile key={index}>
+            <img src={gallery.image} alt={gallery.alt} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </Box>
+  );
+}
+export default ImageCarousel;
+
+const StyledH2 = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: green;
+  height: 150px;
+`;
