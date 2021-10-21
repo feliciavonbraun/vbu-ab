14e1c70fcaca56ae1f4b7b389ccd4c69839b146b
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-10-21 18:41:03 +0200

    add two new interfaces, gallery and services

---
+++ src/components/interfaces/galleryArray.ts
@@ -0,0 +1,42 @@
+import Stallning from "../../assets/stallning.jpg";
+
+export interface Gallery {
+  image: any;
+  alt: string;
+}
+
+const gallerys: Gallery[] = [
+  {
+    image: "https://wallpaperaccess.com/full/1154366.jpg",
+    alt: "",
+  },
+  {
+    image: Stallning,
+    alt: "",
+  },
+  {
+    image: "https://wallpaperaccess.com/full/1154366.jpg",
+    alt: "",
+  },
+  {
+    image: Stallning,
+    alt: "",
+  },
+  {
+    image: "https://wallpaperaccess.com/full/1154366.jpg",
+    alt: "",
+  },
+  {
+    image: Stallning,
+    alt: "",
+  },
+  {
+    image: "https://wallpaperaccess.com/full/1154366.jpg",
+    alt: "",
+  },
+  {
+    image: Stallning,
+    alt: "",
+  },
+];
+export default gallerys;
---
+++ src/components/interfaces/serviceArray.ts
@@ -0,0 +1,47 @@
+export interface Service {
+  item: string;
+}
+
+const services: Service[] = [
+  {
+    item: "Sax & Bomlift (Liftutbildningsrådet).",
+  },
+  {
+    item: "Sax & Bomlift utbildning Repetitionsutbildning & Lärarledd webbutbildning.",
+  },
+  {
+    item: "Fallskydd Grund.",
+  },
+  {
+    item: "Fallskydd Räddning.",
+  },
+  {
+    item: "Brandfarliga Arbeten (Svebra).",
+  },
+  {
+    item: "Truck A1-B6 (Tya).",
+  },
+  {
+    item: "Hjullastare C2 (Tya).",
+  },
+  {
+    item: "Heta Arbeten (Brandskyddsföreningen).",
+  },
+  {
+    item: "Heta Arbeten Repetitionsutbildning & Lärarledd webbutbildning.",
+  },
+  {
+    item: "Teleskoplastare C7 (Tya).",
+  },
+  {
+    item: "Säkra Lyft (Rbk).",
+  },
+  {
+    item: "Traversutbildning.",
+  },
+  {
+    item: "Säkrare Lyft Iso.",
+  },
+];
+
+export default services;
