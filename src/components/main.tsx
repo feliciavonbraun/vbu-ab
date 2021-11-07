d2a0d724295b77ff666b1eb826ff335fd639ed4e
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-11-07 19:19:58 +0100

    fix for netlify

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,6 @@
 import Greeting from "./main/greeting";
 import ImageCarousel from "./main/imageCarousel";
-import Schema from "./main/schema";
+// import Schema from "./main/schema";
 import Services from "./main/services";
 // import PhotoGallery from "./main/photoGallery";
 
@@ -11,8 +11,16 @@ function Main() {
       {/* <PhotoGallery/> */}
       <Services />
       <ImageCarousel />
-      <Schema />
+      {/* <Schema /> */}
     </main>
   );
 }
 export default Main;
+
+// FOR CONTACT FORM WITHOUT DATABASE/SERVER
+// https://medium.com/@patienceadajah/how-to-send-emails-from-a-react-application-without-a-backend-server-1dd8718ceedd
+// https://blog.jakoblind.no/react-forms-hooks/
+// https://www.webtips.dev/react-contact-form-without-backend
+
+// FOR ENTER USER BASED CONTENT WITHOUT DATABASED/SERVER
+// https://www.freecodecamp.org/news/how-to-add-a-serverless-database-to-react-projects-and-web-apps/
