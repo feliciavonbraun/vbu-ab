b2babc29deea562d962c5887bdc3d8200d084a59
Felicia von Braun Wallin <feliciavonbraun@hotmail.com>
2021-12-10 20:50:04 +0100

    extended gallery

--- src/components/interfaces/galleryArray.ts
+++ src/components/interfaces/galleryArray.ts
@@ -3,6 +3,9 @@ import Gallery2 from "../../assets/gallery/gallery2.jpeg";
 import Gallery3 from "../../assets/gallery/gallery3.jpeg";
 import Gallery4 from "../../assets/gallery/gallery4.jpeg";
 import Gallery5 from "../../assets/gallery/gallery5.jpeg";
+import Gallery6 from "../../assets/gallery/gallery6.jpeg";
+import Gallery7 from "../../assets/gallery/gallery7.jpeg";
+import Gallery8 from "../../assets/gallery/gallery8.jpeg";
 
 export interface Gallery {
   image: any;
@@ -28,6 +31,18 @@ const gallerys: Gallery[] = [
   },
   {
     image: Gallery5,
+    alt: "",
+  },
+  {
+    image: Gallery6,
+    alt: "",
+  },
+  {
+    image: Gallery7,
+    alt: "",
+  },
+  {
+    image: Gallery8,
     alt: "",
   },
 ];
