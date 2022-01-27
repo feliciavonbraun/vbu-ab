e1429ca81aafd4546eb5110b8e92d6c66e62d15c
Felicia von Braun Wallin <feliciavonbraun@hotmail.com>
2022-01-27 10:33:50 +0100

    more img´s to gallery

--- src/components/interfaces/galleryArray.ts
+++ src/components/interfaces/galleryArray.ts
@@ -6,6 +6,18 @@ import Gallery5 from "../../assets/gallery/gallery5.jpeg";
 import Gallery6 from "../../assets/gallery/gallery6.jpeg";
 import Gallery7 from "../../assets/gallery/gallery7.jpeg";
 import Gallery8 from "../../assets/gallery/gallery8.jpeg";
+import Gallery9 from "../../assets/gallery/gallery9.jpeg";
+import Gallery10 from "../../assets/gallery/gallery10.jpeg";
+import Gallery11 from "../../assets/gallery/gallery11.jpeg";
+import Gallery12 from "../../assets/gallery/gallery12.jpeg";
+import Gallery13 from "../../assets/gallery/gallery13.jpeg";
+import Gallery14 from "../../assets/gallery/gallery14.jpeg";
+import Gallery15 from "../../assets/gallery/gallery15.jpeg";
+import Gallery16 from "../../assets/gallery/gallery16.jpeg";
+import Gallery17 from "../../assets/gallery/gallery17.jpeg";
+import Gallery18 from "../../assets/gallery/gallery18.jpeg";
+import Gallery19 from "../../assets/gallery/gallery19.jpeg";
+import Gallery20 from "../../assets/gallery/gallery20.jpeg";
 
 export interface Gallery {
   image: any;
@@ -43,6 +55,54 @@ const gallerys: Gallery[] = [
   },
   {
     image: Gallery8,
+    alt: "",
+  },
+  {
+    image: Gallery9,
+    alt: "",
+  },
+  {
+    image: Gallery10,
+    alt: "",
+  },
+  {
+    image: Gallery11,
+    alt: "",
+  },
+  {
+    image: Gallery12,
+    alt: "",
+  },
+  {
+    image: Gallery13,
+    alt: "",
+  },
+  {
+    image: Gallery14,
+    alt: "",
+  },
+  {
+    image: Gallery15,
+    alt: "",
+  },
+  {
+    image: Gallery16,
+    alt: "",
+  },
+  {
+    image: Gallery17,
+    alt: "",
+  },
+  {
+    image: Gallery18,
+    alt: "",
+  },
+  {
+    image: Gallery19,
+    alt: "",
+  },
+  {
+    image: Gallery20,
     alt: "",
   },
 ];
